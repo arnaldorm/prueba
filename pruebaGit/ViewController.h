0254de22c5f29e1b0f8e5f11ed02135e71170d52
Arnaldo Rivera <arnaldorim@gmail.com>
2015-11-26 11:22:04 -0600

    Initial Commit

---
+++ pruebaGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pruebaGit
+//
+//  Created by Arnaldo Rivera on 11/26/15.
+//  Copyright © 2015 Arnaldo Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pruebaGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pruebaGit
+//
+//  Created by Arnaldo Rivera on 11/26/15.
+//  Copyright © 2015 Arnaldo Rivera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
